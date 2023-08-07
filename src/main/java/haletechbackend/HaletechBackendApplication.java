3ee4a205e1c9b6f0cb596a0432b0e4e8c0563583
Soham Chakraborty <soham20chakraborty@gmail.com>
2023-08-07 12:57:30 +0530

    v1

---
+++ src/main/java/haletechbackend/HaletechBackendApplication.java
@@ -0,0 +1,13 @@
+package haletechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaletechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaletechBackendApplication.class, args);
+	}
+
+}

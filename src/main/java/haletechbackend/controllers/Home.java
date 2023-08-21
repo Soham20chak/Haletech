91e193b6007229ee124f965639c06aa40745a3e7
Soham Chakraborty <soham20chakraborty@gmail.com>
2023-08-21 23:15:55 +0530

    v1

--- src/main/java/haletechbackend/controllers/Home.java
+++ src/main/java/haletechbackend/controllers/Home.java
@@ -1,6 +1,9 @@
 package haletechbackend.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class Home {
+    @RequestMapping("/welcome")
     String welcome(){
         String text = "this is a private page";
         text+="This page is not allowed to unauntehenticated users";

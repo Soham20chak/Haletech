1b9c30cdd24a96feb2b8671370e40b052457b2bd
Soham Chakraborty <soham20chakraborty@gmail.com>
2023-08-21 23:14:36 +0530

    v1

--- src/main/java/haletechbackend/controllers/Home.java
+++ src/main/java/haletechbackend/controllers/Home.java
@@ -1,4 +1,9 @@
 package haletechbackend.controllers;
 
 public class Home {
+    String welcome(){
+        String text = "this is a private page";
+        text+="This page is not allowed to unauntehenticated users";
+        return text;
+    }
 }

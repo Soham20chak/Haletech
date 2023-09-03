b745fa1c546c78735fb3396b0eb6bc9510b0386b
Soham Chakraborty <soham20chakraborty@gmail.com>
2023-09-03 13:22:35 +0530

    v1

--- src/main/java/haletechbackend/controllers/PatientController.java
+++ src/main/java/haletechbackend/controllers/PatientController.java
@@ -1,10 +1,15 @@
 package haletechbackend.controllers;
 
 
+import haletechbackend.entities.Patient;
 import haletechbackend.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -12,4 +17,10 @@ public class PatientController {
 
     @Autowired
     private PatientRepository patientRepository;
+
+    @CrossOrigin("http://localhost:4200/")
+    @GetMapping("/doctors")
+    public List<Patient> getAllPatients(){
+        return patientRepository.findAll();
+    }
 }
